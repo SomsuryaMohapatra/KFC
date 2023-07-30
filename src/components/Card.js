a55d726ef075ae094567453cf5469d125fb47254
Somsurya Mohapatra <somsurya.mohapatra2122@gmail.com>
2023-07-30 20:33:07 +0530

    Navbar design optimization & Logout added

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,12 +7,12 @@ export default function Card(props) {
   return (
     <div className="container">
       <div>
-        <div className="card m-2" style={{height:"350px" , width: "18rem", borderRadius: "10px" }}>
+        <div className="card m-2" style={{height:"290px" , width: "18rem" , boxShadow:"5px 5px 5px gray"}}>
           {/* <img src= {process.env.PUBLIC_URL+'/paneer_chilli.jpeg'} alt="paneer chilli"/> */}
           <img
             src={props.imgSrc}
             alt="paneer chilli"
-            style={{ maxHeight: "250px" }}
+            style={{ height: "170px" , objectFit:"fill"}}
           />
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../css/Navbar.css";
 
 export default function Navbar() {
   const location = useLocation();
+  const navigate=useNavigate();
+
+  const handleLogout = ()=>{
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  }
+  const handleLogin = ()=>{
+    navigate("/login");
+  }
+  const handleSignup = ()=>{
+    navigate("/createuser");
+  }
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -27,7 +39,7 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto">
               <li className="nav-item">
                 <Link
                   className={`nav-link ${location === "/" ? "active" : ""}`}
@@ -37,27 +49,37 @@ export default function Navbar() {
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${
-                    location === "/login" ? "active" : ""
-                  }`}
-                  to="/login"
-                >
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className={`nav-link ${
-                    location === "/createuser" ? "active" : ""
-                  }`}
-                  to="/createuser"
-                >
-                  Signup
-                </Link>
-              </li>
+
+              {localStorage.getItem("authToken") && (
+                <li className="nav-item">
+                  <Link
+                    className={`nav-link ${location === "/" ? "active" : ""}`}
+                    aria-current="page"
+                    to="/"
+                  >
+                    My Orders
+                  </Link>
+                </li>
+              )}
             </ul>
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <button className="btn btn-info mx-2 text-white" onClick={handleLogin}>Login</button>
+
+                <button className="btn btn-danger mx-2 text-white" onClick={handleSignup}>
+                  Signup
+                </button>
+              </div>
+            ) : (
+              <div className="d-flex">
+                <button className="btn btn-danger mx-2 text-white" >
+                  My Cart
+                </button>
+                <button className="btn btn-danger mx-2 text-white" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </nav>
